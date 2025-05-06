4023662454d6b9d6e503a2b74ded41b4a6a54589
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2025-05-07 00:40:36 +0200

    Base plugin setup

---
+++ src/main/kotlin/com/jjkay03/statusPlugin/Saves.kt
@@ -0,0 +1,12 @@
+package com.jjkay03.statusPlugin
+
+import net.kyori.adventure.text.format.TextColor
+
+class Saves {
+    companion object {
+        val COLOR_GRAY = TextColor.fromHexString("#808080")
+        val COLOR_RED = TextColor.fromHexString("#ff0014")
+        val COLOR_GREEN = TextColor.fromHexString("#2eec23")
+        val COLOR_PURPLE = TextColor.fromHexString("#8821ff")
+    }
+}
--- src/main/kotlin/com/jjkay03/statusPlugin/StatusPlugin.kt
+++ src/main/kotlin/com/jjkay03/statusPlugin/StatusPlugin.kt
@@ -4,9 +4,22 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class StatusPlugin : JavaPlugin() {
 
+    companion object {
+        lateinit var INSTANCE: StatusPlugin
+    }
+
     // Plugin startup logic
     override fun onEnable() {
+        INSTANCE = this
 
+        // CONFIG STUFF
+        saveDefaultConfig() // Save the default configuration if it doesn't exist
+        reloadConfig() // Reload the configuration
+
+        Saves() // Load all variables in saves class
+
+        // COMMANDS
+        // ...
     }
 
     // Plugin shutdown logic
