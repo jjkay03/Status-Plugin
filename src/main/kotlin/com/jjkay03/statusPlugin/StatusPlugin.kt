83096e77d15bd88e9e1f8c1adecb3513f28dbd51
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2025-05-07 00:03:44 +0200

    Initial commit

---
+++ src/main/kotlin/com/jjkay03/statusPlugin/StatusPlugin.kt
@@ -0,0 +1,16 @@
+package com.jjkay03.statusPlugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class StatusPlugin : JavaPlugin() {
+
+    // Plugin startup logic
+    override fun onEnable() {
+
+    }
+
+    // Plugin shutdown logic
+    override fun onDisable() {
+
+    }
+}
